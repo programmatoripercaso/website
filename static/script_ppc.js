b8feea801b17a5e65a5c6b405847df11bdaad773
mariatdelbove <mariat.delbove@gmail.com>
2021-06-01 18:01:12 +0200

    Cambio logo

--- static/script_ppc.js
+++ static/script_ppc.js
@@ -1,20 +1,19 @@
 var stylesheets = [
-    "stile_ppc.css",
-    "turchese/stile_ppc.css",
+    {foglio_stile: "stile_ppc.css", logo: "Logo_Red_neg.png", logoFooter: "Logo_Red_neg_ext.png"},
+    {foglio_stile: "turchese/stile_ppc.css", logo: "turchese/Logo_Green_neg.png", logoFooter: "turchese/Logo_Green_neg_ext.png"},
 ]
 
-function changeStyleSheet(){
-    console.log(stylesheets)
+function changeStyleSheet(){    
+       var n = Math.floor(Math.random() * stylesheets.length);    
+       var stylesheet = document.getElementById("stylesheet");    
+       let urlo_stylesheet = "static/" + stylesheets[n].foglio_stile;
+       stylesheet.setAttribute('href', urlo_stylesheet);
     
-       var n = Math.floor(Math.random() * stylesheets.length);
-    console.log(n)
+       var logo = document.getElementById("logo")
+       var urlo_logo = "static/" + stylesheets[n].logo;
+       logo.setAttribute('src', urlo_logo);
     
-       var stylesheet = document.getElementById("stylesheet");
-    console.log(stylesheets.length)
-    
-       let urlo = "static/" + stylesheets[n];
-    
-    console.log("url: ", urlo)
-    
-       stylesheet.setAttribute('href', urlo);
-    }
+       var logoFooter = document.getElementById("logo-footer")
+       var urlo_logoFooter = "static/" + stylesheets[n].logoFooter;
+       logoFooter.setAttribute('src', urlo_logoFooter);
+}
