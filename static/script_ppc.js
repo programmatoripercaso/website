da993afaa15469ad787dafba702f965f198ce802
elviraleveque <e_leve@hotmail.it>
2021-07-05 18:49:04 +0200

    Modifiche template stile L_A_B

--- static/script_ppc.js
+++ static/script_ppc.js
@@ -19,8 +19,8 @@ var stylesheets = [
     },
     {
     	foglio_stile: "template_L_A_B/stile_alternativo.css", 
-    	logo: "template_L_A_B/Logo_Red_neg.png", 
-    	logoFooter: "template_L_A_B/Logo_Red_neg_ext.png",
+    	logo: "template_L_A_B/logo-bianco.svg", 
+    	logoFooter: "template_L_A_B/logo-bianco.svg",
     	fonte: "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
     },
     {
