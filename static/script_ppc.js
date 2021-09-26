cbf56b835f9a402b5d77d030b9d424b1dd0d8544
marcellomastrocola <80838694+marcellomastrocola@users.noreply.github.com>
2021-09-26 16:40:31 +0200

    Update script_ppc.js

--- static/script_ppc.js
+++ static/script_ppc.js
@@ -57,7 +57,7 @@ var stylesheets = [
     	foglio_stile: "stile_marcello_mastrocola/stile_alternativo.css", 
     	logo: "template_stile_alternativo/Logo_Green_neg.png", 
     	logoFooter: "template_stile_alternativo/Logo_Green_neg_ext.png",
-	fonte: "https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@300;400;500;600;700;800;900&display=swap"
+	fonte: "https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap"
     },
     // aggiungere qui sopra dopo la virgola la configurazione del nuovo stile
 ]
