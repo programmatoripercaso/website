b7f19b3ec1bb71522d8a52a43d2aa7e497d2c4ff
Lorenzo256 <94621087+Gufoninja@users.noreply.github.com>
2022-09-25 22:55:26 +0200

    Oggetto Ersonland aggiunto

---
+++ ersonland/assets/js/main.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+
+menuBtn.addEventListener('click',() => {
+	menu.style.display = "flex";
+	closeBtn.style.display = "inline-block";
+	menuBtn.style.display = "none"
+})
+
+closeBtn.addEventListener('click',() => {
+	menu.style.display = "none";
+	closeBtn.style.display = "none";
+	menuBtn.style.display = "inline-block"
+})
